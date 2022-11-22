d3d7c3dd3be807fb8406d965b8fd24054557e581
vijaykumarnavulla <vk586072@gmail.com>
2022-11-22 15:43:18 +0530

    adding axios and provider testcase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [data, setData] = React.useState('');
+  const [res, setRes] = React.useState(null);
+  const checkcstate = ()=>{
+    console.log('vkn date ',data);
+    console.log('vkn res ',res);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 data-testid="div-click" onClick={checkcstate}></h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,72 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('app comp describe', ()=>{
+  let setData, setRes;
+  const setSate= jest.fn();
+  const useStateMocks = {
+    [0]:['2323',setSate],
+    [1]:['abc',setSate]};
+
+  it('renders learn react link', async() => {
+   
+
+    jest.spyOn(React,'useState').mockImplementation(()=>{
+      const useStateMock = useStateMocks[0];
+      return useStateMock;
+    })
+    jest.spyOn(React,'useState').mockImplementation(()=>{
+      const useStateMock = useStateMocks[1];
+      return useStateMock;
+    })
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+    const div = await screen.getByTestId('div-click');
+    fireEvent.click(div)
+  });
+
+  fit('render ustsate',async()=>{
+    setData = jest.fn();
+    setRes = jest.fn();
+    jest.spyOn(React,'useState')
+    .mockReturnValueOnce(['abc',()=>{}])
+    .mockReturnValueOnce(['def',()=>{}])
+    render(<App />);
+    const div = await screen.findByTestId('div-click');
+    screen.debug(div);
+    fireEvent.click(div)
+
+  })
+
+  // let url = '';
+  // let body = '';
+  // jest.mock("axios", ()=>({
+  //   post:jest.fn((_url,_body) =>{
+  //     return new Promise((resolve) => {
+  //       url = _url
+  //       body = _body
+  //       resolve(true)
+  //     })
+  //   })
+  // }))
+
+  // const renderweithcontext = (comp) =>{
+  //   return{
+  //     ...render(
+  //       <paycontext.provider value={JSON.parse(sampleJSON)}>
+  //         {comp}
+  //       </paycontext.provider>
+  //     )
+  //   }
+  // }
+  // renderweithcontext(<sampleComp/>);
+  // it('sample teste ', async()=>{
+  //   renderweithcontext(<sampleComp/>);
+  //   (axios.post as jest.MockedFunction<typeof axios.post>).mockReturnValueOnce(() =>
+  //   Promise.resolve(testJSONData));
+  // })
+})
+
