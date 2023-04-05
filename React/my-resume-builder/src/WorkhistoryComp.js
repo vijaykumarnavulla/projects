5e4946c3c2461696657c46fd35d85e26bc40f701
vijaykumarnavulla <vk586072@gmail.com>
2023-04-05 20:49:00 +0530

    added compny history

--- React/my-resume-builder/src/WorkhistoryComp.js
+++ React/my-resume-builder/src/WorkhistoryComp.js
@@ -4,56 +4,56 @@ function WorkhistoryComp() {
 const [workhistory, setWorkhistory] = useState([]);
 useEffect(() => {
   let history = [{
-    designation: 'Lead Consultant', company: 'Virtusa - polaris, Hyderabad', timespan: '2020-20-03 to 2023-04-04',
-    projects: [`I.CXO-billpay — JPMC 
+    designation: 'Lead Consultant', company: 'Virtusa - polaris, Hyderabad', timespan: '2020 Mar 20 to 2023 Apr 04',
+    projects: [{item:`I.CXO-billpay — JPMC 
     CXO ui is a banking application, which is having several modules, in which we are working on bill pay module its having banking add payee and remove payee and see details and request info, it’s basically monolith application  we divided that monolith application into microapp so each modules we are separated micro app and combined to the main app.
-    `, `II.ESG - JPMC, Environment Social and Governance : This is new requirement under TAD seal for enabling users to create various combinations of Product under ESG based on available selections and getting approval for them if neede.
-    `, `III.VES - JPMC, Verification Engineering Services : validating that a Set of Development and Testing practices to increase the overall quality of feature delivery and decrease time to market are completed.These Practices and tooling adoption applies to all Technology stacks as well as modernized and non-modernized application. VES will provide a suite of tools for the teams to use so that the Acceptance Criteria can be met, which will include capabilities for the teams to use their own tooling, and send the data to VDE framework to be used in the Acceptance Criteria step. Complete these tasks to get started `,
-      `IV.GCB - CITI , This is US web internet banking application. In application user can see their bank account, credit card, debit card details. I have worked on the application where user can see personalized offer based on account transcation history.`],
+    `,time:'Time Period : 2022-aug-01 to 2023-apr-04'}, {item:`II.ESG - JPMC, Environment Social and Governance : This is new requirement under TAD seal for enabling users to create various combinations of Product under ESG based on available selections and getting approval for them if neede.
+    `,time:'Time Period : 2022-jan-01 to 2022-july-06'}, {item:`III.VES - JPMC, Verification Engineering Services : validating that a Set of Development and Testing practices to increase the overall quality of feature delivery and decrease time to market are completed.These Practices and tooling adoption applies to all Technology stacks as well as modernized and non-modernized application. VES will provide a suite of tools for the teams to use so that the Acceptance Criteria can be met, which will include capabilities for the teams to use their own tooling, and send the data to VDE framework to be used in the Acceptance Criteria step. Complete these tasks to get started `,time:'Time Period : 2021-apr-04 to 2021-dec-04'},
+      {item:`IV.GCB - CITI , This is US web internet banking application. In application user can see their bank account, credit card, debit card details. I have worked on the application where user can see personalized offer based on account transcation history.`,time:'Time Period : 2020-mar-20 to 2021-apr-04'}],
     skills: 'Reactjs 18, Angular 10, ag-grid, git, jmeter, blazemeter, java, spring boot, jboss, cypress, jest, rtl, tslint, aws'
   },
   {
     designation: 'Delivery Module Lead', company: 'Mphasis, Hyderabad', timespan: '25 nov  2019 to 04 mar 2020',
-    projects: [`I.pmi-projct management information, it is used for employees to get project releated information and employee communicatio to use chatting and video.
+    projects: [{item:`I.pmi-projct management information, it is used for employees to get project releated information and employee communicatio to use chatting and video.
     we have used open-paas tool to create environment for chat and mail.
-      `],
+      `,time:'Time Period : 25 nov 2019 to 04 mar 2020'}],
     skills: 'Angular 9, git, aws,karma,jasmine,spring boot, java, jboss'
   },
   {
     designation: 'Tech Lead', company: 'HCL, Hyderabad', timespan: '30 Oct 2017 to 08 Nov 2019',
-    projects: [`I.ABL — DBS 
+    projects: [{item:`I.ABL — DBS 
     ABL is a banking application, it will help the enduser how to apply for a loan, it will also help the banking staff peoples.It has several roles there (like RM,CRM,ADMIN,OPS,CCU).
-    `, `II.TIW— DBS 
+    `,time:'Time Period : 30 Oct 2018 to 08 Nov 2019'}, {item:`II.TIW— DBS 
     TIW Transaction Initiation Workflow , It's stage wise transaction , for import/export from source to destination.We have total 12 stages (Scan Maker/checker, pre-Processing Maker/Checker , scrutiny,Processing maker/checker,Post processing, QA,Complete) is there to process the end-end transaction complete.
-    `],
+    `,time:'Time Period : 30 Oct 2017 to 08 Nov 2018'}],
     skills: 'Angular 9,Reactjs 16, git, jmeter, blazemeter, java, spring boot, jboss, karma, jasmine, cypress'
   },
   {
-    designation: 'Sr. Software Enginee', company: 'Tech Mahindra, Hyderabad', timespan: '24 Oct 2016 to 09 May 201',
-    projects: [`I.MAGNETONE TEAM 
+    designation: 'Sr. Software Enginee', company: 'Tech Mahindra, Hyderabad', timespan: '24 Oct 2016 to 09 May 2017',
+    projects: [{item:`I.MAGNETONE TEAM 
     Magnet Oneteam is an online space allocation application, where the user can book their meeting rooms/workstation and private space.It has omniture for analytics purposes.
-       `],
+       `,time:'Time Period : 24 Oct 2016 to 09 May 2017'}],
     skills: 'Angular 4, tfs , java , tomcat'
   },
   {
     designation: 'Business Associat', company: 'Crown Solutions, Hyderabad', timespan: '18 Jan 2016 to 21 Jun 2016',
-    projects: [`I.Air2go - it's project related air booking , my role is gathering client requirments fixing the bugs.`],
+    projects: [{item:`I.Air2go - it's project related air booking , my role is gathering client requirments fixing the bugs.`,time:'Time Period : 18 Jan 2016 to 21 Jun 2016'}],
     skills: 'angularjs 1.2,svn,java , tomcat'
   },
   {
     designation: 'Software Engineer', company: 'Tech Mahindra, Hyderabad', timespan: '12 Aug 2013 to 14 Aug 2015',
-    projects: [`I.CONNECT — MSSB
+    projects: [{item:`I.CONNECT — MSSB
     Connect is an IVR application, where the agent can respond to the call and give the end user appropriate bank related information/transactions. End user can transfer the money, and the user can alter the checks.
-       `],
+       `,time:'Time Period : 12 Aug 2013 to 14 Aug 2015'}],
     skills: 'Angular js 1.3, adobe flex, .net , c#, asp.net'
   },
   {
     designation: 'Software Engineer', company: 'Tarento, Bangalore', timespan: '01 Oct 2010 to 04 Feb 2013',
-    projects: [`I.TOUCH GUIDE — CLICKBASE
+    projects: [{item:`I.TOUCH GUIDE — CLICKBASE
     TouchGuide 2.0 is an application running in Kiosk(Touch Screen Systems) at malls. The purpose of this application is to help the customers of the mall to locate shops and services inside the mall. Using this application the user can locate a shop/service on the map. The interactive map will show a path from his/her current location to the destination Shop/service center. TouchGuide is developed as an integrated runtime to develop desktop based RIA (Rich Internet Application). The data is stored in the Kiosk itself and on an interval, the data will be updated from a central Db located over the internet.
-       `, `II.TOUCH ADMIN — CLICKBASE 
+       `,time:'Time Period : 01 Oct 2010 to 04 Feb 2013'}, {item:`II.TOUCH ADMIN — CLICKBASE 
        TouchAdmin is a web application running at malls. The purpose of this application is to store shop information to the TouchServer.The customer can fix the slide show timer for malls, they can upload the shop details data. They can give the Areaid names to the mall's map.The Data stored on the Central server and its update to the local server of the KIOSK.
-       `],
+       `,time:'Time Period : 01 Oct 2010 to 04 Feb 2013'}],
     skills: 'Adobe catalyst, flex 4, flash 5. .net , c# , asp.net'
   }];
   setWorkhistory(history);
@@ -74,7 +74,10 @@ return(
         <span className='compayspan'>{item.company}</span> <span className='timespan'>{item.timespan}</span>
         {item.projects.map(data =>
           <div className='projectdiv'>
-            {data}
+            {data.item}
+            <div className="projectTimediv">
+            {data.time}
+              </div>
           </div>)}
         <div className='divskills'>
           skills: {item.skills}
